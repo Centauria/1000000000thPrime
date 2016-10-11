c7a342de4973876113f5e311623b1e62fced7d20
Centauria <centauria_v_c@163.com>
2016-10-11 23:42:26 +0800

    correct to standard io

--- JuuokuPrime.cpp
+++ JuuokuPrime.cpp
@@ -8,7 +8,7 @@ int _tmain(int argc, char* argv[])
 	LL result = 0;
 	LL currentTotal;
 	LL targetPrimeIndex;
-	time_t t;
+	//time_t t;
 	LL m;
 	bool use_file = false;
 	int i;
@@ -71,13 +71,13 @@ int _tmain(int argc, char* argv[])
 		}
 		if (use_file){
 			m = Get_Last_Prime_from_File(filename) + 2;
-			t = clock();
+			//t = clock();
 			while (1){
 				currentTotal = Get_Length_of_PrimeList_from_File(filename);
-				printf("progress: %u / %u\r", currentTotal + 1, targetPrimeIndex);
+				//printf("progress: %u / %u\r", currentTotal + 1, targetPrimeIndex);
 				if (currentTotal >= targetPrimeIndex - 1){
 					result = Get_nth_Prime_from_File(filename, targetPrimeIndex - 2);
-					putchar('\n');
+					//putchar('\n');
 					break;
 				}
 				Refresh_PrimeTable_in_File(filename, targetPrimeIndex, m);
@@ -85,20 +85,20 @@ int _tmain(int argc, char* argv[])
 		}
 		else{
 			m = prime[prime[0]] + 2;
-			t = clock();
+			//t = clock();
 			while (1){
 				currentTotal = prime[0];
-				printf("progress: %u / %u\r", currentTotal + 1, targetPrimeIndex);
+				//printf("progress: %u / %u\r", currentTotal + 1, targetPrimeIndex);
 				if (currentTotal >= targetPrimeIndex - 1){
 					result = prime[targetPrimeIndex - 1];
-					putchar('\n');
+					//putchar('\n');
 					break;
 				}
 				Refresh_PrimeTable_in_Memory(prime, targetPrimeIndex, m);
 			}
 		}
-		printf("%d ms\n", clock() - t);
-show:	printf("Prime[%u]=%u\n", targetPrimeIndex, result);
+		//printf("%d ms\n", clock() - t);
+show:	printf("%u\n", result);
 	}
 	if (!use_file){
 		delete prime;
