5a3806ffcaff2611f8822452e89b249c1385b39b
Centauria <centauria_v_c@163.com>
2016-10-15 10:23:17 +0800

    input once

--- JuuokuPrime.cpp
+++ JuuokuPrime.cpp
@@ -28,78 +28,81 @@ int _tmain(int argc, char* argv[])
 	}
 	char input[100];
 	char* test;
-	while (gets(input)){
-		if (strcmp(input, "t") == 0){
-			if (use_file){
-				Test_Read_PrimeTable_from_File(filename);
-				continue;
-			}
+	fgets(input, 100, stdin);
+	if (use_file){
+		if (strcmp(input, "t\n") == 0){
+			Test_Read_PrimeTable_from_File(filename);
+			goto end;
 		}
-		if (strlen(input) > 9){
-			printf("Error\n");
-			continue;
+	}
+	if (strlen(input) > 10){
+		printf("Error\n");
+		goto end;
+	}
+	test = input;
+	while (*test){
+		if (*test == '\n'){
+			*test = '\0';
+			break;
 		}
-		test = input;
-		while (*test){
-			if (*test<'0' || *test>'9'){
-				test = NULL;
+		else if (*test<'0' || *test>'9'){
+			test = NULL;
+			break;
+		}
+		test++;
+	}
+	if (test == NULL){
+		printf("Error\n");
+		goto end;
+	}
+	sscanf(input, "%u", &targetPrimeIndex);
+	if (targetPrimeIndex > 100000000){
+		printf("Error\n");
+		goto end;
+	}
+	if (targetPrimeIndex == 0){
+		printf("Error\n");
+		goto end;
+	}
+	if (targetPrimeIndex == 1){
+		result = 2;
+		goto show;
+	}
+	if (targetPrimeIndex < 100){
+		result = primedata[targetPrimeIndex - 1];
+		goto show;
+	}
+	if (use_file){
+		m = Get_Last_Prime_from_File(filename) + 2;
+		//t = clock();
+		while (1){
+			currentTotal = Get_Length_of_PrimeList_from_File(filename);
+			//printf("progress: %u / %u\r", currentTotal + 1, targetPrimeIndex);
+			if (currentTotal >= targetPrimeIndex - 1){
+				result = Get_nth_Prime_from_File(filename, targetPrimeIndex - 2);
+				//putchar('\n');
 				break;
 			}
-			test++;
+			Refresh_PrimeTable_in_File(filename, targetPrimeIndex, m);
 		}
-		if (test == NULL){
-			printf("Error\n");
-			continue;
-		}
-		sscanf(input, "%u", &targetPrimeIndex);
-		if (targetPrimeIndex > 100000000){
-			printf("Error\n");
-			continue;
-		}
-		if (targetPrimeIndex == 0){
-			printf("Error\n");
-			continue;
-		}
-		if (targetPrimeIndex == 1){
-			result = 2;
-			goto show;
-		}
-		if (targetPrimeIndex < 100){
-			result = primedata[targetPrimeIndex - 1];
-			goto show;
-		}
-		if (use_file){
-			m = Get_Last_Prime_from_File(filename) + 2;
-			//t = clock();
-			while (1){
-				currentTotal = Get_Length_of_PrimeList_from_File(filename);
-				//printf("progress: %u / %u\r", currentTotal + 1, targetPrimeIndex);
-				if (currentTotal >= targetPrimeIndex - 1){
-					result = Get_nth_Prime_from_File(filename, targetPrimeIndex - 2);
-					//putchar('\n');
-					break;
-				}
-				Refresh_PrimeTable_in_File(filename, targetPrimeIndex, m);
-			}
-		}
-		else{
-			m = prime[prime[0]] + 2;
-			//t = clock();
-			while (1){
-				currentTotal = prime[0];
-				//printf("progress: %u / %u\r", currentTotal + 1, targetPrimeIndex);
-				if (currentTotal >= targetPrimeIndex - 1){
-					result = prime[targetPrimeIndex - 1];
-					//putchar('\n');
-					break;
-				}
-				Refresh_PrimeTable_in_Memory(prime, targetPrimeIndex, m);
-			}
-		}
-		//printf("%d ms\n", clock() - t);
-show:	printf("%u\n", result);
 	}
-	if (!use_file){
+	else{
+		m = prime[prime[0]] + 2;
+		//t = clock();
+		while (1){
+			currentTotal = prime[0];
+			//printf("progress: %u / %u\r", currentTotal + 1, targetPrimeIndex);
+			if (currentTotal >= targetPrimeIndex - 1){
+				result = prime[targetPrimeIndex - 1];
+				//putchar('\n');
+				break;
+			}
+			Refresh_PrimeTable_in_Memory(prime, targetPrimeIndex, m);
+		}
+	}
+	//printf("%d ms\n", clock() - t);
+show:printf("%u\n", result);
+end:if (!use_file){
 		delete prime;
 	}
 	return 0;
